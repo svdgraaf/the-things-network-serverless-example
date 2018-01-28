7fb8a5195f928c3713e045c692eb833d7b4414b1
Sander van de Graaf <mail@svdgraaf.nl>
2018-01-28 17:44:50 +0100

    Store in dynamo

--- handler.py
+++ handler.py
@@ -1,19 +1,34 @@
 import json
 import os
-import base64
+from base64 import b64decode
 import pprint
 import codecs
+import uuid
+import binascii
+from datetime import datetime
+import boto3
 from binascii import b2a_base64
 from botocore.vendored import requests
-
-key = os.environ.get('TTN_KEY', '')
 pp = pprint.PrettyPrinter(indent=4)
 
 
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+key = os.environ.get('TTN_KEY', '')
+
+
 def uplink(event, context):
-    pp.pprint(event)
     body = json.loads(event['body'])
-    pp.pprint(base64.b64decode(body['payload_raw']))
+
+    item = {
+        'id': str(uuid.uuid1()),
+        'payload': b64decode(body['payload_raw']).decode('utf-8').strip(),
+        'device_id': body['dev_id'],
+        'createdAt': body['metadata']['time'],
+        'updatedAt': body['metadata']['time'],
+    }
+    table.put_item(Item=item)
+
     return {
         "statusCode": 200,
         "body": json.dumps("OK")
@@ -45,12 +60,41 @@ def downlink(event, context):
     return response
 
 
+def log(event, context):
+    data = []
+    response = table.scan()
+    data = response['Items']
+    while response.get('LastEvaluatedKey'):
+        response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+        data.extend(response['Items'])
+
+    data = sorted(data, key=lambda k: k['createdAt'], reverse=True)
+
+    response = {
+        "headers": {
+            "Access-Control-Allow-Headers": "*",
+            "Access-Control-Allow-Origin": "*"
+        },
+        "statusCode": 200,
+        "body": json.dumps(data)
+    }
+    return response
+
 if __name__ == "__main__":
-    pp.pprint(downlink({
-        'body': json.dumps({
-            "payload": "foobar"
-        }),
-        'pathParameters': {
-            'device_id': 'my-test-device'
-        }
-    }, {}))
+    # pp.pprint(log({}, {}))
+    # temps = [b'18.1',b'18.2',b'18.0',b'18.3',b'18.4',b'18.7',b'18.8',b'18.9',b'19.1',b'19.3',b'19.7',b'20.1',b'20.3',b'20.1']
+    # for temp in temps:
+    #     pp.pprint(uplink({
+    #         'body': json.dumps({
+    #             "payload": "foobar",
+    #             "payload_raw": b2a_base64(temp).decode('utf-8').strip(),
+    #             "dev_id": "my-test-device",
+    #             "metadata": {
+    #                 "time": datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+    #             }
+    #         }),
+    #         'pathParameters': {
+    #             'device_id': 'my-test-device'
+    #         }
+    #     }, {}))
+    pass
